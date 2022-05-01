1c4778ad9649da9e015ecfefbb2b9663307f85d9
ngtanloc <nguyentanloc160201@gmail.com>
2022-05-02 00:25:40 +0700

    commit

---
+++ app/src/main/java/com/example/jettip/util/Util.kt
@@ -0,0 +1,13 @@
+package com.example.jettip.util
+
+
+fun calculateTotalTip(totalBill: Double, tipPercentage: Int): Double {
+    return if (totalBill > 1 && totalBill.toString().isNotEmpty())
+        (totalBill * tipPercentage / 100)
+    else 0.0
+}
+
+fun calculateTotalPerPerson(totalBill: Double, tipPercentage: Int, splitBy: Int): Double {
+    var bill = (calculateTotalTip(totalBill, tipPercentage) + totalBill)
+    return bill / splitBy
+}
